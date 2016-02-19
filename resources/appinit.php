9667154184d0c3c50df198d11d179abd458630b1
Mr. Burns <acid24@users.noreply.github.com>
2016-02-19 21:05:35 +0200

    Added some more constants

--- resources/appinit.php
+++ resources/appinit.php
@@ -4,5 +4,7 @@ define('ROOT_DIR', realpath(__DIR__ . '/../'));
 define('VENDOR_DIR', ROOT_DIR . '/vendor');
 define('SRC_DIR', ROOT_DIR . '/src');
 define('CONFIG_DIR', ROOT_DIR . '/config');
+define('RESOURCES_DIR', ROOT_DIR . '/resources');
+define('UNIT_TESTS_DIR', ROOT_DIR . '/tests/unit');
 
 require VENDOR_DIR . '/autoload.php';
