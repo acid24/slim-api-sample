88aae3caf89cc14731004309c184334547a3104b
Mr. Burns <acid24@users.noreply.github.com>
2016-02-19 16:17:04 +0200

    Used initialization file

--- public/index.php
+++ public/index.php
@@ -1,12 +1,6 @@
 <?php
 
-define('ROOT_DIR', realpath(__DIR__ . '/../'));
-define('VENDOR_DIR', ROOT_DIR . '/vendor');
-define('SRC_DIR', ROOT_DIR . '/src');
-define('CONFIG_DIR', ROOT_DIR . '/config');
-
-/** @var \Composer\Autoload\ClassLoader $autoloader */
-$autoloader = require VENDOR_DIR . '/autoload.php';
+require realpath(__DIR__ . '/../resources/appinit.php');
 
 $apiServer = new \Salexandru\Api\Server();
 $apiServer->bootstrap()->run();
