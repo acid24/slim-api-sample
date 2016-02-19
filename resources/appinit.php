e4d20bbb77ae958e4859b7d53e0d1de401396895
Mr. Burns <acid24@users.noreply.github.com>
2016-02-19 16:16:40 +0200

    Moved initialization code into a separate file so that it can be reused

---
+++ resources/appinit.php
@@ -0,0 +1,8 @@
+<?php
+
+define('ROOT_DIR', realpath(__DIR__ . '/../'));
+define('VENDOR_DIR', ROOT_DIR . '/vendor');
+define('SRC_DIR', ROOT_DIR . '/src');
+define('CONFIG_DIR', ROOT_DIR . '/config');
+
+require VENDOR_DIR . '/autoload.php';
