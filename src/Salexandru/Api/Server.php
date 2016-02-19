664c52b44e6b047893c2e1c298024ce5a376941c
Mr. Burns <acid24@users.noreply.github.com>
2016-02-19 20:45:20 +0200

    Added constants for usual API server errors

--- src/Salexandru/Api/Server.php
+++ src/Salexandru/Api/Server.php
@@ -8,6 +8,16 @@ use Salexandru\Api\Server\Bootstrap;
 class Server extends SlimApp
 {
 
+    const ERROR_GENERIC = 1;
+    const ERROR_METHOD_NOT_ALLOWED = 2;
+    const ERROR_RESOURCE_NOT_FOUND = 3;
+    const ERROR_MISSING_CONTENT_TYPE = 4;
+    const ERROR_UNSUPPORTED_MEDIA_TYPE = 5;
+    const ERROR_MALFORMED_INPUT_SYNTAX = 6;
+    const ERROR_MISSING_ACCESS_TOKEN = 7;
+    const ERROR_INVALID_ACCESS_TOKEN = 8;
+    const ERROR_UNEXPECTED_INPUT = 9;
+
     public function bootstrap()
     {
         return new Bootstrap($this);
