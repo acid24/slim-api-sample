e7092386911adb6e433bcd65a705cf052ce2174e
Mr. Burns <acid24@users.noreply.github.com>
2016-02-20 21:02:58 +0200

    Added some routes

--- src/Salexandru/Api/Server/Bootstrapper.php
+++ src/Salexandru/Api/Server/Bootstrapper.php
@@ -2,6 +2,7 @@
 
 namespace Salexandru\Api\Server;
 
+use Interop\Container\ContainerInterface as Container;
 use Salexandru\Api\Server;
 use Salexandru\Api\Server\Exception\Handler\FallbackHandler;
 use Salexandru\Api\Server\Exception\Handler\MethodNotAllowedHandler;
@@ -42,7 +43,16 @@ class Bootstrapper
 
     private function initRoutes()
     {
+        /** @var Container $container */
+        $container = $this->server->getContainer();
 
+        $defaultRequestVetting = $container->get('middleware.requestVetting.default');
+        $tokenlessRequestVetting = $container->get('middleware.requestVetting.noAccessToken');
+
+        $this->server->post('/tokens/actions/issue', $container->get('actions.issueAccessToken'))
+            ->add($tokenlessRequestVetting);
+        $this->server->post('/tokens/actions/refresh', $container->get('actions.refreshAccessToken'))
+            ->add($tokenlessRequestVetting);
     }
 
     private function overrideSlimHandlers()
