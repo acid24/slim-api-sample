500eb1f3ffdae87b2f2731164b32f54e609cb37e
Mr. Burns <acid24@users.noreply.github.com>
2016-02-23 19:00:02 +0200

    Added users seeder

---
+++ resources/db/seeds/UsersSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+class UsersSeeder extends AbstractSeed
+{
+    /**
+     * Run Method.
+     *
+     * Write your database seeder using this method.
+     *
+     * More information on writing seeders is available here:
+     * http://docs.phinx.org/en/latest/seeding.html
+     */
+    public function run()
+    {
+        $faker = Faker\Factory::create();
+        $data = [];
+        for ($i = 0; $i < 100; $i++) {
+            $ts = (new \DateTime())->getTimestamp();
+
+            $data[] = [
+                'username'      => $faker->userName,
+                'password'      => sha1("secret"),
+                'time_created'  => $ts,
+                'last_updated'  => $ts
+            ];
+        }
+
+        $this->insert('users', $data);
+    }
+}
