8e41eb6ca0a2b550ce827674196138d7f99379ad
Mr. Burns <acid24@users.noreply.github.com>
2016-03-03 12:22:31 +0200

    Added an SQL logger for Doctrine

---
+++ src/Salexandru/Db/Logging/DoctrineSqlLogger.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace Salexandru\Db\Logging;
+
+use Doctrine\DBAL\Logging\SQLLogger;
+use Doctrine\DBAL\SQLParserUtils;
+use Doctrine\DBAL\SQLParserUtilsException;
+use Psr\Log\LoggerInterface as PsrLogger;
+
+class DoctrineSqlLogger implements SQLLogger
+{
+
+    /**
+     * @var PsrLogger
+     */
+    private $psrLogger;
+
+    /**
+     * @var float
+     */
+    private $timeStarted;
+
+    public function __construct(PsrLogger $psrLogger)
+    {
+        $this->psrLogger = $psrLogger;
+    }
+
+    /**
+     * Logs a SQL statement somewhere.
+     *
+     * @param string $sql The SQL to be executed.
+     * @param array|null $params The SQL parameters.
+     * @param array|null $types The SQL parameter types.
+     *
+     * @return void
+     */
+    public function startQuery($sql, array $params = null, array $types = null)
+    {
+        if (null !== $params) {
+            $sql = $this->replaceSqlParamsWithValues($sql, $params, $types ?: []);
+        }
+        $this->psrLogger->debug('Received query {sql}', ['sql' => $sql]);
+        $this->timeStarted = microtime(true);
+    }
+
+    /**
+     * Marks the last started query as stopped. This can be used for timing of queries.
+     *
+     * @return void
+     */
+    public function stopQuery()
+    {
+        $timeEnded = microtime(true);
+        $time = $timeEnded - $this->timeStarted;
+
+        $this->psrLogger->debug('Query took {time} seconds', ['time' => $time]);
+    }
+
+    private function replaceSqlParamsWithValues($sql, array $params, array $types)
+    {
+        $hasPositionalParams = is_int(key($params));
+        if ($hasPositionalParams) {
+            list($sql, $params) = SQLParserUtils::expandListParameters($sql, $params, $types);
+        }
+
+        $transformedSql = $sql;
+        $placeholderPositions = SQLParserUtils::getPlaceholderPositions($sql, $hasPositionalParams);
+
+        $deviationAmount = 0;
+        $offset = 0;
+        foreach ($placeholderPositions as $key => $val) {
+            if ($hasPositionalParams) {
+                $paramVal = $params[$offset];
+            } else {
+                $paramVal = isset($params[$val]) ? $params[$val] : $params[":$val"];
+            }
+
+            $paramVal = var_export($paramVal, true);
+
+            $position = ($hasPositionalParams ? $val : $key) + $deviationAmount;
+            $length = $hasPositionalParams ? 1 : strlen($val) + 1;
+
+            $transformedSql = substr_replace($transformedSql, $paramVal, $position, $length);
+            $offset++;
+            $deviationAmount += strlen($paramVal) - $length;
+        }
+
+        return $transformedSql;
+    }
+}
---
+++ tests/unit/Salexandru/Db/Logging/DoctrineSqlLoggerTest.php
@@ -0,0 +1,115 @@
+<?php
+
+namespace Salexandru\Db\Logging;
+
+use Doctrine\DBAL\Connection;
+use Mockery as m;
+use Psr\Log\LoggerInterface as PsrLogger;
+
+class DoctrineSqlLoggerTest extends \PHPUnit_Framework_TestCase
+{
+
+    private $psrLogger;
+
+    protected function setUp()
+    {
+        $this->psrLogger = m::mock(PsrLogger::class);
+    }
+
+    public function testLoggingSqlWithNoParameters()
+    {
+        $sql = 'SELECT * FROM T';
+
+        $expectedMessage = 'Received query {sql}';
+        $expectedContext = ['sql' => $sql];
+
+        $this->psrLogger->shouldReceive('debug')
+            ->with($expectedMessage, $expectedContext)
+            ->getMock();
+
+        $logger = new DoctrineSqlLogger($this->psrLogger);
+        $logger->startQuery($sql);
+    }
+
+    public function testLoggingSqlWithPositionalParameters()
+    {
+        $sql = 'SELECT * FROM T WHERE C1 = ? AND C2 = ? AND C3 != ? AND C4 > ?';
+        $params = [1, 'test', false, 0.01];
+
+        $transformedSql = "SELECT * FROM T WHERE C1 = 1 AND C2 = 'test' AND C3 != false AND C4 > 0.01";
+
+        $expectedMessage = 'Received query {sql}';
+        $expectedContext = ['sql' => $transformedSql];
+
+        $this->psrLogger->shouldReceive('debug')
+            ->with($expectedMessage, $expectedContext)
+            ->getMock();
+
+        $logger = new DoctrineSqlLogger($this->psrLogger);
+        $logger->startQuery($sql, $params);
+    }
+
+    public function testLoggingSqlWithInClause()
+    {
+        $sql = 'SELECT * FROM T WHERE C1 = ? AND C2 IN (?)';
+        $params = [true, ['test', 'pest', 'best']];
+        $types = [\PDO::PARAM_BOOL, Connection::PARAM_STR_ARRAY];
+
+        $transformedSql = "SELECT * FROM T WHERE C1 = true AND C2 IN ('test', 'pest', 'best')";
+
+        $expectedMessage = 'Received query {sql}';
+        $expectedContext = ['sql' => $transformedSql];
+
+        $this->psrLogger->shouldReceive('debug')
+            ->with($expectedMessage, $expectedContext)
+            ->getMock();
+
+        $logger = new DoctrineSqlLogger($this->psrLogger);
+        $logger->startQuery($sql, $params, $types);
+    }
+
+    public function testLoggingSqlWithNamedParameters()
+    {
+        $sql = 'SELECT * FROM T WHERE C1 = :c1 AND C2 = :c2 AND C3 != :c3 AND C4 > :c4';
+        $params = ['c1' => 1, 'c2' => 'test', 'c3' => false, 'c4' => 0.01];
+
+        $transformedSql = "SELECT * FROM T WHERE C1 = 1 AND C2 = 'test' AND C3 != false AND C4 > 0.01";
+
+        $expectedMessage = 'Received query {sql}';
+        $expectedContext = ['sql' => $transformedSql];
+
+        $this->psrLogger->shouldReceive('debug')
+            ->with($expectedMessage, $expectedContext)
+            ->getMock();
+
+        $logger = new DoctrineSqlLogger($this->psrLogger);
+        $logger->startQuery($sql, $params);
+    }
+
+    public function testQueryExecutionTimeIsLogged()
+    {
+        $sql = 'SELECT * FROM T';
+
+        $expectedMessage = 'Received query {sql}';
+        $expectedContext = ['sql' => $sql];
+
+        $this->psrLogger->shouldReceive('debug')
+            ->with($expectedMessage, $expectedContext);
+
+        $expectedMessage = 'Query took {time} seconds';
+        $expectedContext = \Mockery::on(function (array $context) {
+            if (!isset($context['time'])) {
+                return false;
+            }
+
+            return is_float($context['time']) && $context['time'] > 0;
+        });
+
+        $this->psrLogger->shouldReceive('debug')
+            ->with($expectedMessage, $expectedContext);
+
+        $logger = new DoctrineSqlLogger($this->psrLogger);
+        $logger->startQuery($sql);
+        $logger->stopQuery();
+    }
+}
