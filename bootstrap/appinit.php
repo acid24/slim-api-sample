6c605e5389ae1d8b15aad1b917f601e197207b8c
Mr. Burns <acid24@users.noreply.github.com>
2016-03-06 19:15:50 +0200

    Extracted constants declaration into a separate file

--- bootstrap/appinit.php
+++ bootstrap/appinit.php
@@ -1,21 +1,4 @@
 <?php
 
-// Root directory of the project
-define('ROOT_DIR', realpath(__DIR__ . '/../'));
-
-// Vendor directory of the project
-define('VENDOR_DIR', ROOT_DIR . '/vendor');
-
-// Sources directory of the project
-define('SRC_DIR', ROOT_DIR . '/src');
-
-// Resources directory of the project
-define('RESOURCES_DIR', ROOT_DIR . '/resources');
-
-// Configuration directory of the project
-define('CONFIG_DIR', ROOT_DIR . '/config');
-
-// Unit tests directory of the project
-define('UNIT_TESTS_DIR', ROOT_DIR . '/tests/unit');
-
+require 'constants.php';
 require VENDOR_DIR . '/autoload.php';
